75a2a865ed88d01b9c0b3e815d034063deac339d
Gaurav Gaur <gauravgaur5697@gmail.com>
2021-01-02 19:24:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+{/* We NEED React Router for */}
+{/*localhost.com/loginPage */}
+{/*localhost.com/checkout and etc whatever pages we want to add */}
+{/*It is used for adding multipage functionality in singlepage */}
+import { BrowserRouter as Router, Switch, Route } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="app">
+      <h1>Online Shopping</h1>
+      <Switch>
+        <Route path="/checkout"><h1>CheckOut</h1></Route> {/* This is the route for checkout page*/ }
+        <Route path="/login"><h1>LogIn</h1></Route>  {/* This is the route for login page*/ }
+        <Route path="/"><h1>HOME PAGE!!!</h1></Route> {/* This is the route for default home page of default route*/ }
+      </Switch>
     </div>
+    </Router>
+    
   );
 }
 
 export default App;
+
